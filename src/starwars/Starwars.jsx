b22f43e9b629e86bc02190805459625e9576ad9c
sergio <stortosa01@gmail.com>
2019-09-13 17:36:55 +0200

    first route other way

--- src/starwars/Starwars.jsx
+++ src/starwars/Starwars.jsx
@@ -1,29 +1,61 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 export default class Starwars extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filmList: []
+    };
+
+  }
+
+  getFilms = () => {
+
+    axios.get(`https://swapi.co/api/films/`)
+
+      .then(response => {
+        this.setState({
+          //metemos los "results" en filmList luego solo poner el dato que queremos sacar
+          filmList: response.data.results
+        })
+        console.log(response.data.results)
+        // console.log(this.state.filmList)
+      })
+
+  }
+
+
+
+  componentDidMount() {
+    this.getFilms();
+  }
+
   render() {
     return (
       <div>
         <Link to="/planets">
-            <li>Planets</li>
+          <li>Planets</li>
         </Link>
         <Link to="/people">
-            <li>People</li>
+          <li>People</li>
         </Link>
         <Link to="/species">
-            <li>Species</li>
+          <li>Species</li>
         </Link>
         <Link to="/vehicles">
-            <li>Vehicles</li>
+          <li>Vehicles</li>
         </Link>
         <Link to="/starships">
-            <li>Starships</li>
+          <li>Starships</li>
         </Link>
         <h1>Films of Star Wars:</h1>
 
-      </div>
+      <ul>{this.state.filmList.map(film => <li>{film.title}</li>)}</ul>
+      
+      </div >
     )
   }
 }
