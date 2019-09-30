531139709c96068ee63182dfac41ee7b9776f2f4
sergio <stortosa01@gmail.com>
2019-09-30 18:45:55 +0200

    try elements

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,34 +13,40 @@ export default class Home extends Component {
         <div>
           <section className="sec1">
             <h1>STARWARS UNIVERSE</h1>
-            <Link style={navStyle} to="/starwars">
-              <button>Star Wars Films</button>
-            </Link>
-            <Link style={navStyle} to="/planets">
-              <button>Planets</button>
-            </Link>
+            {/* <div className="btn-home">
+              <Link style={navStyle} to="/starwars">
+                <button className="starbtn">Star Wars Films</button>
+              </Link>
+              <Link style={navStyle} to="/planets">
+                <button className="starbtn">Planets</button>
+              </Link>
+            </div> */}
           </section>
           <section className="secText">
-            <h1>VISIT THE PEOPLE AND SPECIES THAT APPEAR INTO THE FILM:</h1>
+            <h1>VISIT THE PEOPLE AND SPECIES THAT APPEAR INTO THE FILM</h1>
           </section>
           <section className="sec2">
-            <Link style={navStyle} to="/people">
-              <button>People</button>
-            </Link>
-            <Link style={navStyle} to="/species">
-              <button>Species</button>
-            </Link>
+            {/* <div>
+              <Link style={navStyle} to="/people">
+                <button className="starbtn">People</button>
+              </Link>
+              <Link style={navStyle} to="/species">
+                <button className="starbtn">Species</button>
+              </Link>
+            </div> */}
           </section>
           <section className="secText">
             <h1>VEHICLES AND STARSHIPS ABOUT STARWARS</h1>
           </section>
           <section className="sec3">
-            <Link style={navStyle} to="/vehicles">
-              <button>Vehicles</button>
-            </Link>
-            <Link style={navStyle} to="/starships">
-              <button>Starships</button>
-            </Link>
+            {/* <div>
+              <Link style={navStyle} to="/vehicles">
+                <button className="starbtn">Vehicles</button>
+              </Link>
+              <Link style={navStyle} to="/starships">
+                <button className="starbtn">Starships</button>
+              </Link>
+            </div> */}
           </section>
           <section className="secText"></section>
         </div>
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,35 +5,39 @@ import { Link } from 'react-router-dom';
 export default class Nav extends Component {
   render() {
     const navStyle = {
-      color: 'white'
+      color: 'rgb(245, 246, 51)',
+      width: '70%',
+      display: 'flex',
+      margin: '5px',
+      padding: '5px',
     };
 
     return (
       <nav>
         <ul className="nav-links">
-        <Link style={navStyle} to="/">
-          <li>Home</li>
-        </Link>
-        <Link style={navStyle} to="/starwars">
-          <li>Star Wars</li>
-        </Link>
-        <Link style={navStyle} to="/planets">
-          <li>Planets</li>
-        </Link>
-        <Link style={navStyle} to="/people">
+          <Link style={navStyle} to="/">
+            <li>Home</li>
+          </Link>
+          <Link style={navStyle} to="/starwars">
+            <li>Star Wars</li>
+          </Link>
+          <Link style={navStyle} to="/planets">
+            <li>Planets</li>
+          </Link>
+          <Link style={navStyle} to="/people">
             <li>People</li>
-        </Link>
-        <Link style={navStyle} to="/species">
+          </Link>
+          <Link style={navStyle} to="/species">
             <li>Species</li>
-        </Link>
-        <Link style={navStyle} to="/vehicles">
+          </Link>
+          <Link style={navStyle} to="/vehicles">
             <li>Vehicles</li>
-        </Link>
-        <Link style={navStyle} to="/starships">
+          </Link>
+          <Link style={navStyle} to="/starships">
             <li>Starships</li>
-        </Link>
+          </Link>
         </ul>
-      </nav>
+      </nav >
     )
   }
 }
